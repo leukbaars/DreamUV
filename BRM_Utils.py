cf647d7efa0595b6deaa2f958de6268a6acf9efd
Jeiel Aranal <jeiel.aranal@gmail.com>
2017-08-06 15:25:04 +0800

    Added face UV axis function to BRM_Utils

--- BRM_Utils.py
+++ BRM_Utils.py
@@ -1,5 +1,76 @@
-import bpy
+import sys
 from mathutils import Vector
+
+
+def get_face_uv_axis(face, uv_layer, world_matrix, view_up_vector, view_right_vector):
+    """
+    Find the UV translation axis by matching the face edges to view up and right vectors
+    :param face:
+    :param uv_layer:
+    :param world_matrix:
+    :param view_up_vector:
+    :param view_right_vector:
+    :return:
+    """
+    uv_x = None
+    uv_y = None
+    closest_up = sys.float_info.max
+    closest_right = sys.float_info.max
+    closest_dot_up = None
+    closest_dot_right = None
+
+    # Start from index 1, so we can can just
+    # go to previous loop to get an edge
+    for o, loop in enumerate(face.loops, 1):
+        prev_loop = loop.link_loop_prev
+
+        # Calculate the edge in world space, so matches view up and right vectors
+        edge_vec = (world_matrix * loop.vert.co) - (world_matrix * prev_loop.vert.co)
+        edge_vec.normalize()
+        # Calculate the UV vector back to previous vertex
+        uv_vec = loop[uv_layer].uv - prev_loop[uv_layer].uv
+        # Calculate dot against view vectors, to get similarity
+        dot_up = edge_vec.dot(view_up_vector)
+        dot_right = edge_vec.dot(view_right_vector)
+        # Restrict dot to 0-1 space for convenience
+        flat_up = 1.0 - abs(dot_up)
+        flat_right = 1.0 - abs(dot_right)
+
+        if flat_up < closest_up:
+            # If dot up is negative, reverse UV vector
+            # if dot_up < 0:
+            #     uv_vec *= -1
+            # Bigger axis is chosen as the vector
+            if abs(uv_vec.y) > abs(uv_vec.x):
+                # Lazy normalization, basically
+                uv_y = Vector((0, 1 if uv_vec.y > 0 else -1))
+            else:
+                uv_y = Vector((1 if uv_vec.x > 0 else -1, 0))
+
+            closest_up = flat_up
+            closest_dot_up = dot_up
+
+        if flat_right < closest_right:
+            # if dot_right < 0:
+            #     uv_vec *= -1
+            # Bigger axis is chosen as the vector
+            if abs(uv_vec.y) > abs(uv_vec.x):
+                uv_x = Vector((0, 1 if uv_vec.y > 0 else -1))
+            else:
+                uv_x = Vector((1 if uv_vec.x > 0 else -1, 0))
+
+            closest_right = flat_right
+            closest_dot_right = dot_right
+
+    if uv_x is None or uv_y is None:
+        return None
+    # Fail safe, to make sure a weird parallel axis wasn't constructed
+    if abs(uv_x.dot(uv_y)) > 0.7:
+        return None
+    # Normalize the dots, so value of 1 is the
+    # closest axis to both view up and view right
+    normalized_dot = abs(closest_dot_right + closest_dot_up) / 2
+    return uv_x, uv_y, normalized_dot
 
 
 def get_face_pixel_step(context, face):
